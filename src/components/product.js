0d65db5e3874229d8da8a3a4ebedd675f864bbfd
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-09-05 10:24:40 +0200

    Update product.js

--- src/components/product.js
+++ src/components/product.js
@@ -37,55 +37,61 @@ const handleCheckboxChange = () => {
   const isMachineGrain = ['Nos Grains','Nos Promos','Capsules Lavazza Firma','Capsules Lavazza Blue'].includes(product.Designation_Famille_Stat1)
   return (
  
-    <div className='mt-3 col-12 col-md-12 cart-product'>         
-           <div style={{ backgroundColor: '#f3f3f3', borderTop: '0px solid #fffff', width:'100%', padding:'0px !important'}} className='mt-1 shop-card  bg-body'>
-          <div onClick={handleShow}>
-            <img src={product.Image} alt='product' className='img' style={{ height: '140px', width: '110px', overflow:'hidden', backgroundColor:'#f3f3f3 !important' }} />
-          </div>         
-              <div className="product-tag1 justify-content-end">
+  <div className='mt-1 col-12 col-md-12 cart-product'>         
+    <div style={{ backgroundColor: '#f3f3f3', borderTop: '0px solid #ffffff', width:'100%', padding:'0px !important' }} className='shop-card bg-body d-flex align-items-center'>
+      
+      <div style={{ width: '20%' }} onClick={handleShow}>
+        <img src={product.Image} alt='product' className='img' style={{ height: '70px', width: '50px', overflow:'hidden', backgroundColor:'#f3f3f3 !important' }} />
+      </div>
+  
+      <div className="product-tag1 d-flex align-items-center justify-content-between" style={{ width: '80%' }}>
+        <div >        <h3 className='pt-2 text-start block' style={{ fontSize:'12px',color:'#1b6cfc', width:'30%' }}>{product.ITMREF_0}</h3>
+        <h6 className='text-start' style={{ fontSize:'7px',color:'black',paddingRight:'45px' }}>{product.ITMDES1_0}</h6>
+</div>
+       
+      
+        
+        <div className='flex-container check product-quantity text-start mt-3' >
+          <input
+            type='number'
+            className='form-control'
+            style={{ width: '35px' ,marginRight:'8px'}}  
+            value={quantity}
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
+              if (isNaN(value)) {
+                setQuantity('');
+              } else {
+                setQuantity(Math.max(value, 1));
+              }
+            }} 
+            min={1} 
+          />  
+         
+            <input
+              type='checkbox'
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+              className=''
+             
+            />  <label className='grat-title mx-1 px-0' style={{ fontSize: '8px',paddingTop:'20px'}}>{isMachineCategory ? 'Dépots?' : ' Gratuit?'}
+          </label> 
+        </div>
+  
+        <div className='product-price mt-4' style={{ width: '160px' ,color:'black',fontSize:'9px',paddingTop:'10px'}}>
+          <h6 style={{ color:'#1b6cfc',fontSize:'10px'}}>{calculatedPrice} DH TTC</h6>
+        </div>
+        <div className="product-tag1 justify-content-end mt-4">
               <img 
       src={imageSrc} 
       alt='ADD' 
-      style={{ height: '18px', cursor: 'pointer' }} 
+      style={{ height: '18px', cursor: 'pointer',width:'18px' }} 
       onClick={handleClick} 
     />
 							</div> 
-        </div>    
-        <h3 className='pt-2 px-3 text-truncate text-start' style={{ fontSize:'10px',color:'#1b6cfc'}}>{product.ITMREF_0}</h3>
-    
-          <h6 className='pt-2 px-3 text-start text-truncate' style={{ fontSize:'11px'}}>{product.ITMDES1_0}</h6>
-            <div className='flex-container check  product-quantity'> 
-         
-            <input
-              type='number'
-              className='form-control'
-              value={quantity}
-              onChange={(e) => {
-                const value = parseInt(e.target.value);
-                if (isNaN(value)) {
-                  setQuantity('');
-                } else {
-                  setQuantity(Math.max(value, 1));
-                }
-              }} 
-              min={1} 
-            />  
-               <label className='text-start grat-title'>
-              <input
-                
-                type='checkbox'
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-                className=''
-              /> {isMachineCategory ? 'Dépots ?' : '  Gratuit ?'}
-          
-            </label> 
-       </div>
-          <div className='flex-container'>
-            <div className='product-price'>
-              <h3>{calculatedPrice.toLocaleString('fr-MA', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} DH HT</h3>
-            </div>
-          </div> 
-      </div>
+      </div> 
+  
+    </div>    
+  </div>
   );
 }
